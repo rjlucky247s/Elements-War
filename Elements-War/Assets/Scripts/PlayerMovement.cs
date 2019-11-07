18aa3697bdce34a7640880b257f150789f356a65
rjlucky247s <rjlucky247s@gmail.com>
2019-11-06 19:39:15 -0500

    Player Animation added

--- Elements-War/Assets/Scripts/PlayerMovement.cs
+++ Elements-War/Assets/Scripts/PlayerMovement.cs
@@ -1,43 +1,55 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public CharacterController controller;
+public class PlayerMovement : MonoBehaviour {
 
-    public float runSpeed = 40f;
+	public CharacterController2D controller;
+	public Animator animator;
 
-    float horizontalMove = 0f;
-    bool jump = false;
-    bool crouch = false;
+	public float runSpeed = 40f;
 
-    // Update is called once per frame
-    void Update()
-    {
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	// Update is called once per frame
+	void Update () {
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        }
-        else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        }
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-    }
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
 
-    void FixedUpdate()
-    {
-        // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
-    }
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
