91b6f3b3bd57c194a9a24ba77ccacf6fb9cebdbc
gillkashdeep <gillkashdeep@gmail.com>
2019-11-07 20:28:46 -0500

    level_1

---
+++ Elements-War/Assets/Scripts/enemy script/enemyMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float closedistance = 5f;
+    public float speed = 4f;
+    Rigidbody2D rb;
+    public Animator animator;
+
+    public float health = 100f;
+   
+  
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Distance = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 dist = player.transform.position - transform.position;
+
+
+        if (Distance < closedistance)
+        {
+            rb.velocity = dist * 1f;
+            animator.SetBool("walk", true);
+            if (rb.velocity.x >= 0.01f)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x <= -0.01f)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        if (Distance > closedistance)
+        {
+            rb.velocity = dist * 0f;
+            animator.SetBool("walk", false);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitinfo)
+    {
+
+        if (hitinfo.gameObject.CompareTag("bullet")) {
+            health -= 25f;
+            if (health <= 0)
+            {
+
+                animator.SetBool("death", true);
+                Destroy(gameObject,0.45f);
+            }
+        }
+       
+
+    }
+}
---
+++ Elements-War/Assets/Scripts/shooting/bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 20;
+    public Rigidbody2D rb;
+    public GameObject impacteffect;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hitinfo)
+    {
+        
+        
+
+        Destroy(Instantiate(impacteffect, transform.position, transform.rotation),0.1f);
+        Destroy(gameObject);
+      
+       
+        
+
+    }
+
+
+
+}
